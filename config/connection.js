9c7b58136a07b3feda30ecdfeeb816750201db6a
Anna <a.gondring@gmail.com>
2019-01-11 11:13:40 -0500

    Intiaial commit

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+Inside the connection.js file, setup the code to connect Node to MySQL.
+
+
+Export the connection.
+
+
+
+
---
+++ db/burgers_db.sql
@@ -0,0 +1,8 @@
+Create a burgers table with these fields:
+
+
+id: an auto incrementing int that serves as the primary key.
+
+burger_name: a string.
+
+devoured: a boolean.
